5b163e00fda64b0dbf308738ad599fdb84de0590
Esau21 <esaumoran199@gmail.com>
2022-12-28 12:09:35 -0600

    tareas panel odooV.13

---
+++ webinar_todo/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name":"Webinar Prueba",
+    "description":"Esta aplicacion te permitira registrar todas tus actividades",
+    "data":[
+        'views.xml',
+        'security.xml'
+    ]
+}
